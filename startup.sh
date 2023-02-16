3a88369a072e0573b27fa278bb840dc56550292d
mcol <mcol@posteo.net>
2023-02-16 18:45:39 +0000

    update startup script

--- startup.sh
+++ startup.sh
@@ -2,49 +2,51 @@
 #
 # Qtile Wayland startup script
 
-{
-    foot --server &
+foot --server &
 
-    # wob
-    FIFO=/tmp/wob-$WAYLAND_DISPLAY
-    test -e $FIFO || mkfifo $FIFO
-    tail -f $FIFO | wob \
-	-a bottom -M 0 -H 28 -W 1920 -o 0 -b 0 -p 0 \
-	--background-color "#00000000" --bar-color "#66CFCCD6" -t 800 &
-
-} &> /dev/null
+# Wallpaper
+(
+    swww init
+    sleep 1
+    swww img --filter Nearest --transition-step=1 --transition-fps 60 \
+	--transition-duration 12 ~/pictures/Wallpapers/Leuh6wm-slow.gif
+) &
 
 run_if_new() { ps aux | grep -v grep | grep -q $1 || $@; }
 
 [[ -z "$QTILE_XEPHYR" ]] && {
     # Session setup
     systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
-    dbus-update-activation-environment --systemd \
-	WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP &
+    dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
 
     # Services
-    kanshi &>> ~/.local/share/qtile/qtile.log &
+    kanshi  &
     wlsunset &
     swayidle &
     swaync &
     mpDris2 &
     sway-mpris-idle-inhibit &
     playerctld daemon &
+    nm-applet --indicator &
     #kdeconnect-indicator &
+    darkman run &
 
     # Startup programs
-    #run_if_new firefox &
-    #run_if_new geary &
+    run_if_new keepassxc &
+    sleep 2
+    run_if_new firefox &
+    run_if_new evolution &
 
     # Notify me if any systemd services failed
     check_systemd &
-} &> /dev/null
+
+# Send all output to Qtile's log
+} &>> ~/.local/share/qtile/qtile.log
 
 
 # Clean up
 clean() {
     pkill -P $$
-    test -e $FIFO && rm $FIFO
 }
 trap clean SIGINT SIGTERM
 sleep infinity &
